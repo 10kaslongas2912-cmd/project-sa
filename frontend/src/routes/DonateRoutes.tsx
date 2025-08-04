419108753f0e0f6cdd183cb6c2b976039327cbe1
siridet-s <siridet.s.dev@email.com>
2025-08-04 16:09:55 +0700

    edit1

--- frontend/src/routes/DonateRoutes.tsx
+++ frontend/src/routes/DonateRoutes.tsx
@@ -2,13 +2,13 @@
 import { lazy } from 'react';
 import type { RouteObject } from "react-router-dom";
 import DonateLayout from "../layout/DonateLayout"; // Component ที่ใช้สำหรับ Layout ของหน้านี้ 
-import DonationMoneyForm from "../pages/donate/DonationMoney";
-import DonationConfirmationPage from "../pages/donate/DonationConfirmationPage";
+import DonationMoneyForm from "../pages/public/donate/DonationMoney";
+import DonationConfirmationPage from "../pages/public/donate/DonationConfirmationPage";
 import Loadable from "../components/third-patry/Loadable";
 
-const DonationOptionsPage = Loadable(lazy(() => import("../pages/donate/DonationOptionsPage")));
-const InformationDonorsPage = Loadable(lazy(() => import("../pages/donate/InformationDonors")));
-const DonatePage = Loadable(lazy(() => import("../pages/donate")));
+const DonationOptionsPage = Loadable(lazy(() => import("../pages/public/donate/DonationOptionsPage")));
+const InformationDonorsPage = Loadable(lazy(() => import("../pages/public/donate/InformationDonors")));
+const DonatePage = Loadable(lazy(() => import("../pages/public/donate")));
 const DonateRoutes = (): RouteObject => {
   return {
     path: 'donate', 
